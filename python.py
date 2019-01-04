443fb5ae5eb5774f7b576587d78aa9039dccc0b2
tahirdemir <46366591+tahirdemir@users.noreply.github.com>
2019-01-04 10:08:18 +0300

    Add files via upload

---
+++ python.py
@@ -0,0 +1,14 @@
+#a = input ("1.sayıyı giriniz :")
+#b = input ("2.sayıyı giriniz :")
+#c = input ("3.sayıyı giriniz :")
+#print ("sayıların toplamı",int(a)+int(b)+int(c))
+print("Aile Tablosuna Hoş Geldiniz :) ")
+abla=input("Abla Adı Giriniz :")
+kardes=input("Kardeş Adı Giriniz :")
+print("LODİNG....".readline())
+
+list=[abla,kardes]
+print("Yükleme Tamamlandı Enter tuşuna Basınız...")
+print("Abla Adı :",abla)
+print("Kardeş Adı :",kardes)
+print("Teşekkürler")
